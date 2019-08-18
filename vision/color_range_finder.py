6f1cbcd74ca0584239d36c08818e661c5b9afbf0
Nils Egger <nils.egger@stud.bbbaden.ch>
2019-08-18 14:04:32 +0200

    color range and circle detection tool camera update

--- vision/color_range_finder.py
+++ vision/color_range_finder.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
-img = cv2.imread("cap/test.PNG")
+resolution = (1024, 768)
+camera = PiCamera()
+camera.resolution = resolution
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=resolution)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+time.sleep(1)
+
+# img = cv2.imread("cap/test.PNG")
 
 cv2.namedWindow("mask")
 cv2.namedWindow("hsv")
@@ -60,6 +69,7 @@ param1 = 50
 param2 = 14
 max_radius = 0
 
+
 def on_min_dist(val):
     global min_dist
     min_dist = min_dist
@@ -68,27 +78,45 @@ def on_min_dist(val):
 def on_param1(val):
     global param1
     param1 = val
+    if param1 < 1:
+        param1 = 1
 
 
 def on_param2(val):
     global param2
     param2 = val
+    if param2 < 1:
+        param2 = 1
+
 
 def on_max_radius(val):
     global max_radius
     max_radius = val
+
 
 cv2.createTrackbar("min_dist", "img", min_dist, 100, on_min_dist)
 cv2.createTrackbar("param1", "img", param1, 500, on_param1)
 cv2.createTrackbar("param2", "img", param2, 500, on_param2)
 cv2.createTrackbar("maxRadius", "img", max_radius, 500, on_max_radius)
 
-while cv2.getWindowProperty('mask', cv2.WND_PROP_VISIBLE) > 0 and cv2.getWindowProperty('hsv',
-                                                                                        cv2.WND_PROP_VISIBLE) > 0 and cv2.getWindowProperty(
-    'img',
-    cv2.WND_PROP_VISIBLE) > 0:
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
-    img_copy = img.copy()
+    windows = ["img", "hsv", "mask"]
+
+    for window in windows:
+        if cv2.getWindowProperty(window, cv2.WND_PROP_VISIBLE) < 1:
+            break
+
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    img = frame.array
+
+    img = cv2.flip(img, 0)
+    img = cv2.flip(img, 1)
+    # show the frame
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     lower_range = np.array([lb, lg, lr])
     upper_range = np.array([ub, ug, ur])
@@ -100,12 +128,19 @@ while cv2.getWindowProperty('mask', cv2.WND_PROP_VISIBLE) > 0 and cv2.getWindowP
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for circle in circles[0, :]:
-            cv2.circle(img_copy, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
+            cv2.circle(img, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
 
     cv2.imshow("hsv", hsv)
     cv2.imshow("mask", mask)
-    cv2.imshow("img", img_copy)
-    cv2.waitKey(16)
+    cv2.imshow("img", img)
+    key = cv2.waitKey(1)
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 
 cv2.destroyAllWindows()
 cv2.waitKey(1)
